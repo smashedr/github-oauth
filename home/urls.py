7bf2c597a96af31c33326a1c5b03029c0f5730f0
Shane <shane@shanerice.me>
2016-12-18 17:17:31 -0800

    Cleaned up code and added extras

--- home/urls.py
+++ home/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     url(r'^$', home.home, name='home'),
     url(r'^favicon\.ico$', RedirectView.as_view(url=STATIC_URL + 'favicon.ico')),
     url(r'^callback/', home.callback, name='callback'),
-    url(r'^doauth/', home.doauth, name='doauth'),
     url(r'^admin/', admin.site.urls, name="django_admin"),
 ]
--- home/views.py
+++ home/views.py
@@ -15,73 +15,46 @@ def home(request):
     })
 
 
-def doauth(request):
-    # View: /doauth/
-    pgdata = {'client_id': config.get('GitHub', 'client_id')}
-    return render(request, 'doauth.html', {
-        'pgdata': pgdata,
-    })
-
-
 def callback(request):
     # View: /callback/
     pgdata = {'success': False}
-    if 'code' in request.GET and 'state' in request.GET:
+    if 'code' in request.GET:
         code = request.GET['code']
-        state = request.GET['state']
         try:
             results = github_token(code)
-            # save_token('shane', results)
-            access_token = results['access_token'][0]
-            ship_it(state, access_token)
-            if ship_it:
-                pgdata['ghdata'] = {
-                    'code': code,
-                    'state': state,
-                    'access_token': access_token,
+            if 'access_token' in results:
+                pgdata['gh'] = {
+                    'token': results['access_token'],
+                    'scope': results['scope'],
                 }
                 pgdata['success'] = True
+            elif 'error_description' in results:
+                pgdata['error'] = results['error_description']
             else:
-                pgdata['error'] = 'Unable to verify request.'
+                pgdata['error'] = 'Unable to parse GitHub response data.'
         except Exception as error:
             logger.exception(error)
             pgdata['error'] = error
     else:
-        pgdata['error'] = 'Unable to parse: code or state'
+        pgdata['error'] = 'Unable to parse code from query string.'
 
     return render(request, 'callback.html', {
         'pgdata': pgdata,
     })
 
 
-def ship_it(state, access_token):
-    try:
-        logger.info(state + ' - ' + access_token)
-        url = 'http://bot02.cssnr.com/github-token/'
-        data = {
-            'state': state,
-            'access_token': access_token,
-        }
-        r = requests.post(url=url, data=data)
-        response = r.json()
-        if response['success']:
-            return True
-        else:
-            return False
-    except Exception as error:
-        logger.exception(error)
-        return False
-
-
 def github_token(code):
+    """
+    Post OAuth code to GitHub and parse response data
+    """
     uri = 'https://github.com/login/oauth/access_token'
     data = {
         'client_id': config.get('GitHub', 'client_id'),
         'client_secret': config.get('GitHub', 'client_secret'),
         'code': code,
     }
-    r = requests.post(uri, data=data)
-    logger.info(r.text)
-    results = parse_qs(r.text)
+    headers = {'Accept': 'application/json'}
+    r = requests.post(uri, data=data, headers=headers)
+    results = r.json()
     logger.info(results)
     return results
